e93509efe8198684f061febaabe54a9ec69e15f7
cobobc <ciaranob4@gmail.com>
2021-08-18 21:12:20 +0100

    remove spacing to test for potential error

--- assets/js/quiz_special.js
+++ assets/js/quiz_special.js
@@ -60,25 +60,24 @@
         choices: ["Jordan Speith", "Ricky Fowler", "Sergio Garcia", "Nick Faldo"],
         correctAnswer: 2
     }];
-​
 
     let questionCounter = 0; //Tracks question number
     let selections = []; //Array containing user choices
     const quiz = $('#quiz'); //Quiz div object
-​
+
     // Display initial question
     displayNext();
-​
+
     // Click handler for the 'next' button
     $('#next').on('click', function (e) {
         e.preventDefault();
-​
+
         // Suspend click listener during fade animation
         if(quiz.is(':animated')) {
             return false;
         }
         choose();
-​
+
         // If no user selection, progress is stopped
         if (isNaN(selections[questionCounter])) {
             alert('In golf you need to put the ball in the hole to progress. Please select an answer to proceed!');
@@ -87,11 +86,10 @@
             displayNext();
         }
     });
-​
+
     // Click handler for the 'prev' button
     $('#prev').on('click', function (e) {
         e.preventDefault();
-​
         if(quiz.is(':animated')) {
             return false;
         }
@@ -99,11 +97,10 @@
         questionCounter--;
         displayNext();
     });
-​
+
     // Click handler for the 'Start Over' button
     $('#start').on('click', function (e) {
         e.preventDefault();
-​
         if(quiz.is(':animated')) {
             return false;
         }
@@ -112,34 +109,31 @@
         displayNext();
         $('#start').hide();
     });
-​
+
     // Animates buttons on hover
     $('.button').on('mouseenter', function () {
         $(this).addClass('active');
     });
+
     $('.button').on('mouseleave', function () {
         $(this).removeClass('active');
     });
-​
+
     // Creates and returns the div that contains the questions and 
     // the answer selections
     function createQuestionElement(index) {
         let qElement = $('<div>', {
             id: 'question'
         });
-​
         let header = $('<h2>Question ' + (index + 1) + ':</h2>');
         qElement.append(header);
-​
         let question = $('<p>').append(questions[index].question);
         qElement.append(question);
-​
         let radioButtons = createRadios(index);
         qElement.append(radioButtons);
-​
         return qElement;
     }
-​
+
     // Creates a list of the answer choices as radio inputs
     function createRadios(index) {
         let radioList = $('<ul>');
@@ -154,29 +148,27 @@
         }
         return radioList;
     }
-​
+
     // Reads the user selection and pushes the value to an array
     function choose() {
         selections[questionCounter] = +$('input[name="answer"]:checked').val();
     }
-​
+
     // Displays next requested element
     function displayNext() {
         quiz.fadeOut(function() {
             $('#question').remove();
-​
             if(questionCounter < questions.length){
                 let nextQuestion = createQuestionElement(questionCounter);
                 quiz.append(nextQuestion).fadeIn();
                 if (!(isNaN(selections[questionCounter]))) {
                     $('input[value='+selections[questionCounter]+']').prop('checked', true);
                 }
-​
+
                 // Controls display of 'prev' button
                 if(questionCounter === 1){
                     $('#prev').show();
                 } else if(questionCounter === 0){
-​
                     $('#prev').hide();
                     $('#next').show();
                 }
@@ -189,18 +181,16 @@
             }
         });
     }
-​
+    
     // Calculates the score and returns a paragraph element to be displayed
     function displayScore() {
         let score = $('<p>',{id: 'question'});
-​
         let numCorrect = 0;
         for (let i = 0; i < selections.length; i++) {
             if (selections[i] === questions[i].correctAnswer) {
                 numCorrect++;
             }
         }
-​
         score.append('Congratulations, you got ' + numCorrect + ' questions out of ' +
             questions.length + ' correct! See you at the Ryder Cup! ');
         return score;
