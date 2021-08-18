1ae700424de912c07d6a5a1b4ebec3c96f4ca972
cobobc <ciaranob4@gmail.com>
2021-08-18 19:10:21 +0000

    Remove zero width characters that interupt site

--- assets/js/quizzer.js
+++ assets/js/quizzer.js
@@ -80,24 +80,19 @@
         choices: ["Adam Scott", "Charl Schwartzel", "Bubba Watson", "Angel Cabrera", "Danny Willett"],
         correctAnswer: 4
     }];
-​
     let questionCounter = 0; //Tracks question number
     let selections = []; //Array containing user choices
     const quiz = $('#quiz'); //Quiz div object
-​
     // Display initial question
     displayNext();
-​
     // Click handler for the 'next' button
     $('#next').on('click', function (e) {
         e.preventDefault();
-​
         // Suspend click listener during fade animation
         if(quiz.is(':animated')) {
             return false;
         }
         choose();
-​
         // If no user selection, progress is stopped
         if (isNaN(selections[questionCounter])) {
             alert('In golf you need to put the ball in the hole to progress. Please select an answer to proceed!');
@@ -106,11 +101,9 @@
             displayNext();
         }
     });
-​
     // Click handler for the 'prev' button
     $('#prev').on('click', function (e) {
         e.preventDefault();
-​
         if(quiz.is(':animated')) {
             return false;
         }
@@ -118,11 +111,9 @@
         questionCounter--;
         displayNext();
     });
-​
     // Click handler for the 'Start Over' button
     $('#start').on('click', function (e) {
         e.preventDefault();
-​
         if(quiz.is(':animated')) {
             return false;
         }
@@ -131,7 +122,6 @@
         displayNext();
         $('#start').hide();
     });
-​
     // Animates buttons on hover
     $('.button').on('mouseenter', function () {
         $(this).addClass('active');
@@ -139,26 +129,20 @@
     $('.button').on('mouseleave', function () {
         $(this).removeClass('active');
     });
-​
     // Creates and returns the div that contains the questions and 
     // the answer selections
     function createQuestionElement(index) {
         let qElement = $('<div>', {
             id: 'question'
         });
-​
         let header = $('<h2>Question ' + (index + 1) + ':</h2>');
         qElement.append(header);
-​
         let question = $('<p>').append(questions[index].question);
         qElement.append(question);
-​
         let radioButtons = createRadios(index);
         qElement.append(radioButtons);
-​
         return qElement;
     }
-​
     // Creates a list of the answer choices as radio inputs
     function createRadios(index) {
         let radioList = $('<ul>');
@@ -173,29 +157,24 @@
         }
         return radioList;
     }
-​
     // Reads the user selection and pushes the value to an array
     function choose() {
         selections[questionCounter] = +$('input[name="answer"]:checked').val();
     }
-​
     // Displays next requested element
     function displayNext() {
         quiz.fadeOut(function() {
             $('#question').remove();
-​
             if(questionCounter < questions.length){
                 let nextQuestion = createQuestionElement(questionCounter);
                 quiz.append(nextQuestion).fadeIn();
                 if (!(isNaN(selections[questionCounter]))) {
                     $('input[value='+selections[questionCounter]+']').prop('checked', true);
                 }
-​
                 // Controls display of 'prev' button
                 if(questionCounter === 1){
                     $('#prev').show();
                 } else if(questionCounter === 0){
-​
                     $('#prev').hide();
                     $('#next').show();
                 }
@@ -208,18 +187,15 @@
             }
         });
     }
-​
     // Calculates the score and returns a paragraph element to be displayed
     function displayScore() {
         let score = $('<p>',{id: 'question'});
-​
         let numCorrect = 0;
         for (let i = 0; i < selections.length; i++) {
             if (selections[i] === questions[i].correctAnswer) {
                 numCorrect++;
             }
         }
-​
         score.append('Congratulations, you got ' + numCorrect + ' questions out of ' +
             questions.length + ' correct! You should try your skills out of the golf course! ');
         return score;
