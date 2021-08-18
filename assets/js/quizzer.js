0e79c59b03a3df42c4d6e0513bd718e78b012498
cobobc <ciaranob4@gmail.com>
2021-08-18 20:58:52 +0100

    add return into quizzer.js

--- assets/js/quizzer.js
+++ assets/js/quizzer.js
@@ -80,11 +80,14 @@
         choices: ["Adam Scott", "Charl Schwartzel", "Bubba Watson", "Angel Cabrera", "Danny Willett"],
         correctAnswer: 4
     }];
+
     let questionCounter = 0; //Tracks question number
     let selections = []; //Array containing user choices
     const quiz = $('#quiz'); //Quiz div object
+
     // Display initial question
     displayNext();
+
     // Click handler for the 'next' button
     $('#next').on('click', function (e) {
         e.preventDefault();
@@ -101,6 +104,7 @@
             displayNext();
         }
     });
+
     // Click handler for the 'prev' button
     $('#prev').on('click', function (e) {
         e.preventDefault();
@@ -111,6 +115,7 @@
         questionCounter--;
         displayNext();
     });
+
     // Click handler for the 'Start Over' button
     $('#start').on('click', function (e) {
         e.preventDefault();
@@ -122,6 +127,7 @@
         displayNext();
         $('#start').hide();
     });
+
     // Animates buttons on hover
     $('.button').on('mouseenter', function () {
         $(this).addClass('active');
@@ -129,6 +135,7 @@
     $('.button').on('mouseleave', function () {
         $(this).removeClass('active');
     });
+
     // Creates and returns the div that contains the questions and 
     // the answer selections
     function createQuestionElement(index) {
@@ -143,6 +150,7 @@
         qElement.append(radioButtons);
         return qElement;
     }
+
     // Creates a list of the answer choices as radio inputs
     function createRadios(index) {
         let radioList = $('<ul>');
@@ -157,10 +165,12 @@
         }
         return radioList;
     }
+
     // Reads the user selection and pushes the value to an array
     function choose() {
         selections[questionCounter] = +$('input[name="answer"]:checked').val();
     }
+
     // Displays next requested element
     function displayNext() {
         quiz.fadeOut(function() {
@@ -171,6 +181,7 @@
                 if (!(isNaN(selections[questionCounter]))) {
                     $('input[value='+selections[questionCounter]+']').prop('checked', true);
                 }
+
                 // Controls display of 'prev' button
                 if(questionCounter === 1){
                     $('#prev').show();
@@ -187,6 +198,7 @@
             }
         });
     }
+    
     // Calculates the score and returns a paragraph element to be displayed
     function displayScore() {
         let score = $('<p>',{id: 'question'});
